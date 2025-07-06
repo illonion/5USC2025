d2de7b71ef3a7f0f72cbee39c0e3897284efb3f6
Lawrence Zhou <60532041+illonion@users.noreply.github.com>
2025-07-06 20:01:56 +1000

    Set round information

--- main/index.js
+++ main/index.js
@@ -5,6 +5,19 @@ const leftTeamNameEl= document.getElementById("left-team-name")
 const rightTeamNameEl = document.getElementById("right-team-name")
 let currentLeftTeamName, currentRightTeamName
 
+// Beatmap information
+const roundNameEl = document.getElementById("round-name")
+let allBeatmaps
+async function getBeatmaps() {
+    const response = await axios.get("../_data/beatmaps.json")
+    allBeatmaps = response.data.beatmaps
+
+    roundNameEl.textContent = response.data.roundName
+}
+getBeatmaps()
+// Find Beatmaps
+const findBeatmaps = beatmapId => allBeatmaps.find(beatmap => Number(beatmap.beatmap_id) === Number(beatmapId))
+
 // Socket
 const socket = createTosuWsSocket()
 socket.onmessage = event => {
