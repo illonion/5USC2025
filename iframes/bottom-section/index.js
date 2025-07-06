5a3081658819f2bb9396a96b49d040f336944755
Lawrence Zhou <60532041+illonion@users.noreply.github.com>
2025-07-06 22:52:27 +1000

    Complete chat

--- iframes/bottom-section/index.js
+++ iframes/bottom-section/index.js
@@ -16,6 +16,10 @@ const nowPlayingSongArtistEl = document.getElementById("now-playing-song-artist"
 // Variables
 let currentId, currentChecksum
 
+// Chat Display
+const liveChatContainerEl = document.getElementById("live-chat-container")
+let chatLen = 0
+
 // Socket
 const socket = createTosuWsSocket()
 socket.onmessage = event => {
@@ -37,5 +41,36 @@ socket.onmessage = event => {
         } else {
             nowPlayingModIdEl.style.display = "none"
         }
+    }
+
+    // This is also mostly taken from Victim Crasher: https://github.com/VictimCrasher/static/tree/master/WaveTournament
+    if (chatLen !== data.tourney.chat.length) {
+        (chatLen === 0 || chatLen > data.tourney.chat.length) ? (liveChatContainerEl.innerHTML = "", chatLen = 0) : null
+        const fragment = document.createDocumentFragment()
+
+        for (let i = chatLen; i < data.tourney.chat.length; i++) {
+            const chatColour = data.tourney.chat[i].team
+
+            // Chat message container
+            const liveChatWrapper = document.createElement("div")
+            liveChatWrapper.classList.add("live-chat-wrapper")  
+
+            // Name
+            const liveChatName = document.createElement("span")
+            liveChatName.classList.add(chatColour)
+            liveChatName.innerText = data.tourney.chat[i].name + ": ";
+
+            // Message
+            const liveChatMessage = document.createElement("span")
+            liveChatMessage.classList.add("message-content")
+            liveChatMessage.innerText = data.tourney.chat[i].message
+
+            liveChatWrapper.append(liveChatName, liveChatMessage)
+            fragment.append(liveChatWrapper)
+        }
+
+        liveChatContainerEl.append(fragment)
+        chatLen = data.tourney.chat.length
+        liveChatContainerEl.scrollTop = liveChatContainerEl.scrollHeight
     }
 }
