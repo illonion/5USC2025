e8a6dddf6e26343d3e664f447121104d51d57400
Lawrence Zhou <60532041+illonion@users.noreply.github.com>
2025-07-09 17:26:27 +1000

    Add team information

--- mappool/index.js
+++ mappool/index.js
@@ -10,3 +10,25 @@ async function getBeatmaps() {
 getBeatmaps()
 // Find Beatmaps
 const findBeatmaps = beatmapId => allBeatmaps.find(beatmap => Number(beatmap.beatmap_id) === Number(beatmapId))
+
+// Team Inforamtion
+const leftTeamNameEl= document.getElementById("left-team-name")
+const rightTeamNameEl = document.getElementById("right-team-name")
+let currentLeftTeamName, currentRightTeamName
+
+// Socket
+const socket = createTosuWsSocket()
+socket.onmessage = event => {
+    const data = JSON.parse(event.data)
+    console.log(data)
+
+    // Team information
+    if (currentLeftTeamName !== data.tourney.team.left) {
+        currentLeftTeamName = data.tourney.team.left
+        leftTeamNameEl.textContent = currentLeftTeamName
+    }
+    if (currentRightTeamName !== data.tourney.team.right) {
+        currentRightTeamName = data.tourney.team.right
+        rightTeamNameEl.textContent = currentRightTeamName
+    }
+}
